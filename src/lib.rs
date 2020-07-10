18e3fa0b4e2054a2a987bd4032a33c208ccee66b
Timothy Andrew <mail@timothyandrew.net>
2020-07-10 12:04:42 +0530

    special-case youtube

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,17 @@ use std::error::Error;
 use regex::Regex;
 
 pub async fn get_title(url: &str) -> Result<String, Box<dyn Error>> {
-  // TODO: Stop fetching once we see `</title>`
+  // TODO: Use an actual HTML parser
+  let regex = if url.contains("youtube.com") {
+    Regex::new("\"twitter:title\" content=\"([^\"]*)\"")?
+  } else {
+    Regex::new("<title[^>]*>([^<]*)</title>")?
+  };
+
+  // TODO: Stop fetching once we see a boundary
   let text = reqwest::get(url).await?.text().await?;
-  let title = Regex::new("<title[^>]*>([^<]*)</title>")?.captures(&text).unwrap().get(1).unwrap();
+
+  let title = regex.captures(&text).unwrap().get(1).unwrap();
   let title = title.as_str().to_owned();
   let title = title.replace("\n", " ");
 
